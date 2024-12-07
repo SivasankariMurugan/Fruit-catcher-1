acc99fb631e2d44678ab8c8e2f618a8ef3f97717
sreehariniR <harinisree.ramesh@gmail.com>
2024-12-07 23:04:41 +0530

    Add falling objects and collision detection

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pgzrun
 from pygame.transform import scale
+import random
 
 
 WIDTH = 600
@@ -11,11 +12,20 @@ game_active = False  # Game state to determine if the game should run
 
 
 basket = Actor("basket", (WIDTH // 2, HEIGHT - 50))
+falling_objects = []
+fruits = ["apple", "banana", "strawberry"]
+bombs = ["bomb1", "bomb2"]
+speed = 2
+score = 0
+
+
 
 def draw():
     if game_active:
         screen.fill(BACKGROUND_COLOR)
         basket.draw()       
+        for obj in falling_objects:
+            obj.draw()
     else: 
         screen.fill(BACKGROUND_COLOR)
         screen.draw.filled_rect(Rect((250, 90), (100, 40)), BUTTON_COLOR)
@@ -23,7 +33,9 @@ def draw():
         screen.draw.text("Fruit Catcher", center=(300, 50), fontsize=40, color=TEXT_COLOR)
 
 def update():
-    global game_active
+    global game_active, speed, score
+
+    print("SCORE:", score)
 
     if not game_active:
         return
@@ -33,6 +45,34 @@ def update():
         basket.x -= 5
     if keyboard.right and basket.x < WIDTH - 70:
         basket.x += 5
+
+     # Update falling objects
+    for obj in falling_objects:
+        obj.y += speed
+
+        # Check if object is caught by basket
+        if obj.y >= basket.top and obj.colliderect(basket):
+            print("SCORE:", score)
+            if obj.image in fruits:
+                score += 10
+            elif obj.image in bombs:
+                score -= 5
+            falling_objects.remove(obj)
+
+        elif obj.y > HEIGHT:
+            falling_objects.remove(obj)
+
+       # Spawn new objects
+    if random.randint(1, 30) == 1:
+        if random.randint(1,4)>1:
+            new_object = Actor(random.choice(fruits), (random.randint(50, WIDTH - 50), 0))
+            if not any(new_object.colliderect(obj) for obj in falling_objects):
+                falling_objects.append(new_object)
+        else:
+            new_object = Actor(random.choice(bombs), (random.randint(50, WIDTH - 50), 0))
+            if not any(new_object.colliderect(obj) for obj in falling_objects):
+                falling_objects.append(new_object)
+    
 
 def on_mouse_down(pos):
     global game_active
